ed970f4afc0eef9a9d4302902b992cc5b5de5994
MizaelDouglas <mizaeldouglas2@gmail.com>
2025-02-28 00:13:50 -0300

    feat: add TaskStatusDto

---
+++ src/TaskManagement.Application/DTOs/TaskStatusDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagement.Application.DTOs
+{
+    public class TaskStatusDto
+    {
+        public string Pending { get; set; } = string.Empty;
+        public string InProgress { get; set; } = string.Empty;
+        public string Completed { get; set; } = string.Empty;
+    }
+}
