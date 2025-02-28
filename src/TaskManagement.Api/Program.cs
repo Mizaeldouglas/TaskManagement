b1f709658e26599463218e476771316a78d45b53
MizaelDouglas <mizaeldouglas2@gmail.com>
2025-02-28 00:17:25 -0300

    chore: configure services and middlewares in Program.cs

--- src/TaskManagement.Api/Program.cs
+++ src/TaskManagement.Api/Program.cs
@@ -1,54 +1,85 @@
+using System.Reflection;
+using System.Text.Json.Serialization;
+using TaskManagement.Application;
+using TaskManagement.Infrastructure;
+using TaskManagement.Api.Middleware;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using TaskManagement.Domain.Exceptions;
+using TaskManagement.Api.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo
+    c.SwaggerDoc("v1", new() { Title = "Task Management API", Version = "v1" });
+    c.DocumentFilter<CustomSchemaFilter>();
+    c.MapType<ValidationException>(() => new OpenApiSchema
     {
-        Title = "Task Management API",
-        Version = "v1",
-        Description = "Task Management API"
+        Type = "object",
+        Properties =
+        new Dictionary<string, OpenApiSchema>
+        {
+            ["message"] = new OpenApiSchema { Type = "string" }
+        }
+    });
+
+    c.UseAllOfToExtendReferenceSchemas();
+    c.UseAllOfForInheritance();
+
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
     });
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<TaskManagement.Infrastructure.Persistence.ApplicationDbContext>();
+            context.Database.Migrate();
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred while migrating the database.");
+        }
+    }
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseCors("AllowAll");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
